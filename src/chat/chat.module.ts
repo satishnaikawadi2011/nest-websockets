a923e44fa642580af8dcf7a0342bfcc82131360e
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-08-08 18:43:28 +0530

    Made ws server to work by adding adapter.

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ChatGateway } from './chat.gateway';
 
-@Module({})
+@Module({
+	providers:
+		[
+			ChatGateway
+		]
+})
 export class ChatModule {}
